e01f0fd06769739025d1ad7b7251fda20ff9f899
Waly FALL <2atelecompwf@gmail.com>
2026-08-20 21:54:47 +0000

    deployed

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,15 @@
 import { io } from "socket.io-client";
 
+const socket = io("https://senecole-back.onrender.com", {
+  withCredentials: true,
+  transports: ["websocket", "polling"],
+});
+
+export default socket;
+/*import { io } from "socket.io-client";
+
 
 const socket = io("http://localhost:8989");
 
 
-export default socket;
+export default socket;*/
