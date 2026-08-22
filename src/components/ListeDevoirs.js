75047222f747a3376bded1a3d8335990c9c20385
Waly FALL <2atelecompwf@gmail.com>
2026-08-22 12:16:25 +0000

    live-eleves

--- src/components/ListeDevoirs.js
+++ src/components/ListeDevoirs.js
@@ -47,7 +47,8 @@ const ListeDevoirs = ({devoirsClasse, handleDeleteDevoirs, API_URL }) => {
                   {c.fichiers.map((f, i) => (
                     <a
                       key={i}
-                      href={`${API_URL.replace("/api", "")}/${f.url}`}
+                      href={f.url}
+                      //href={`${API_URL.replace("/api", "")}/${f.url}`}
                       target="_blank"
                       rel="noopener noreferrer"
                       style={{
@@ -90,20 +91,6 @@ const ListeDevoirs = ({devoirsClasse, handleDeleteDevoirs, API_URL }) => {
     🗑 Supprimer
   </button>
 )}
-
-              {/*<button
-                onClick={() => handleDeleteDevoirs(c._id)}
-                style={{
-                  marginTop: "8px",
-                  border: "none",
-                  padding: "5px 8px",
-                  borderRadius: "5px",
-                  cursor: "pointer",
-                  fontSize: "12px",
-                }}
-              >
-                🗑 Supprimer
-              </button>*/}
             </li>
           ))}
         </ul>
