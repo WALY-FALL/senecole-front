508ccb37589239dea33d570b72f9848009e521df
Waly FALL <2atelecompwf@gmail.com>
2026-09-01 22:04:56 +0000

    liste

--- src/components/ListeExercicesEleve.js
+++ src/components/ListeExercicesEleve.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import ListeExercices from "./ListeExsercices";
+import ListeExercices from "./ListeExercices";
 
 const API_URL =
   process.env.REACT_APP_API_URL || "http://localhost:8989/api";
